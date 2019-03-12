8fdac5b9c29fd3ff2786ed769ad6bb04d63af5b2
enriquefdez5 <uo257742@uniovi.es>
2019-03-12 16:56:33 +0100

    loading popul tests

---
+++ src/app/login-popup/login-popup.component.spec.ts
@@ -0,0 +1,32 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginPopupComponent } from './login-popup.component';
+
+describe('DashboardComponent', () => {
+  let component: LoginPopupComponent;
+  let fixture: ComponentFixture<LoginPopupComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LoginPopupComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginPopupComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should be there an element with loading text', () => {
+    const fixture = TestBed.createComponent(LoginPopupComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#app-container').textContent).toContain('Loading...');
+  });
+});
